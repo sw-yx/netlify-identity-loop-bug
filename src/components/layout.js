a487389a2c5f289b2759058394e41dd54d375d82
sw-yx <swyx@dontemail.me>
2019-06-10 00:21:56 -0400

    put in rest of widget

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,14 +1,37 @@
 import React from "react"
 import { useIdentityContext } from "react-netlify-identity-widget"
 
+// code split the modal til you need it!
+const IdentityModal = React.lazy(() => import("react-netlify-identity-widget"))
+
 const Layout = ({ children }) => {
   const identity = useIdentityContext()
+  const [dialog, setDialog] = React.useState(false)
+  const name =
+    (identity &&
+      identity.user &&
+      identity.user.user_metadata &&
+      identity.user.user_metadata.name) ||
+    "NoName"
 
   console.log(JSON.stringify(identity))
-
+  const isLoggedIn = identity && identity.isLoggedIn
   return (
     <>
+      <nav style={{ background: "green" }}>
+        {" "}
+        Login Status:
+        <button className="btn" onClick={() => setDialog(true)}>
+          {isLoggedIn ? `Hello ${name}, Log out here!` : "LOG IN"}
+        </button>
+      </nav>
       <main>{children}</main>
+      <React.Suspense fallback="loading...">
+        <IdentityModal
+          showDialog={dialog}
+          onCloseDialog={() => setDialog(false)}
+        />
+      </React.Suspense>
     </>
   )
 }
